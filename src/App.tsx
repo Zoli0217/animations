25616541fc102d304972b2a7f1328f76cbdbfc2f
Zoli0217 <zoltanbodi61@gmail.com>
2026-03-31 13:34:05 +0200

    revolver animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import EmojiWrapper from './components/Emoji/EmojiWrapper'
 import AnimatedBorder from './components/AnimatedBorder/AnimatedBorder'
+import Revolver from './components/RevolverMenu/Revolver'
 
 const App = () => {
   return (
     <div>
       <EmojiWrapper/>
       <AnimatedBorder/>
+      <Revolver />
     </div>
   )
 }
---
+++ src/components/RevolverMenu/Revolver.tsx
@@ -0,0 +1,11 @@
+import styles from "./Revolver.module.css"
+
+const Revolver = () => {
+  return (
+    <div className={styles.revolver}>
+        
+    </div>
+  )
+}
+
+export default Revolver
